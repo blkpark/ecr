70ad5225deb2cded26f19ce1add2fdcbfd74637b
Graham Lee <ghmlee@ghmlee.com>
2019-10-05 08:01:56 -0700

    Fix indent

--- main.go
+++ main.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "time"
-  "errors"
-  "strings"
-  "encoding/base64"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
 
-  "github.com/aws/aws-sdk-go/aws"
-  "github.com/aws/aws-sdk-go/aws/credentials"
-  "github.com/aws/aws-sdk-go/aws/session"
-  "github.com/aws/aws-sdk-go/service/ecr"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ecr"
 )
 
 const (
-  RETRY = 1
+	RETRY = 1
 )
 
 type parameter struct {
-  key string
-  secret string
-  region string
-  registry string
-  retry int
+	key      string
+	secret   string
+	region   string
+	registry string
+	retry    int
 }
 
 type auth struct {
-	username string
-	password string
-	proxy string
+	username   string
+	password   string
+	proxy      string
 	expiration time.Time
 }
 
@@ -43,120 +43,120 @@ func env(key, val string) string {
 }
 
 func getParameter() (*parameter, error) {
-  key := env("KEY", "")
-  secret := env("SECRET", "")
-  region := env("REGION", "")
-  registry := env("REGISTRY", "")
+	key := env("KEY", "")
+	secret := env("SECRET", "")
+	region := env("REGION", "")
+	registry := env("REGISTRY", "")
 
-  var err error
+	var err error
 
-  for {
-    if key == "" {
-      err = errors.New("Invalid parameter for KEY")
-      break
-    }
+	for {
+		if key == "" {
+			err = errors.New("Invalid parameter for KEY")
+			break
+		}
 
-    if secret == "" {
-      err = errors.New("Invalid parameter for SECRET")
-      break
-    }
+		if secret == "" {
+			err = errors.New("Invalid parameter for SECRET")
+			break
+		}
 
-    if region == "" {
-      err = errors.New("Invalid parameter for REGION")
-      break
-    }
+		if region == "" {
+			err = errors.New("Invalid parameter for REGION")
+			break
+		}
 
-    if registry == "" {
-      err = errors.New("Invalid parameter for REGISTRY")
-      break
-    }
+		if registry == "" {
+			err = errors.New("Invalid parameter for REGISTRY")
+			break
+		}
 
-    break // success
-  }
+		break // success
+	}
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  p := &parameter{
-    key: key,
-    secret: secret,
-    region: region,
-    registry: registry,
-    retry: RETRY,
-  }
+	p := &parameter{
+		key:      key,
+		secret:   secret,
+		region:   region,
+		registry: registry,
+		retry:    RETRY,
+	}
 
-  return p, nil
+	return p, nil
 }
 
 func getAuths(p *parameter) ([]*auth, error) {
-  s, err := session.NewSession(&aws.Config{
-    Region: aws.String(p.region),
-    Credentials: credentials.NewStaticCredentials(p.key, p.secret, ""),
-  })
+	s, err := session.NewSession(&aws.Config{
+		Region:      aws.String(p.region),
+		Credentials: credentials.NewStaticCredentials(p.key, p.secret, ""),
+	})
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  input := &ecr.GetAuthorizationTokenInput{
-    RegistryIds: []*string{aws.String(p.registry)},
-  }
+	input := &ecr.GetAuthorizationTokenInput{
+		RegistryIds: []*string{aws.String(p.registry)},
+	}
 
 	e := ecr.New(s, aws.NewConfig().WithMaxRetries(p.retry).WithRegion(p.region))
 
-  res, err := e.GetAuthorizationToken(input)
-  if err != nil {
-    return nil, errors.New("No token")
-  }
+	res, err := e.GetAuthorizationToken(input)
+	if err != nil {
+		return nil, errors.New("No token")
+	}
 
-  auths := []*auth{}
+	auths := []*auth{}
 
 	for _, v := range res.AuthorizationData {
 		token, err := base64.StdEncoding.DecodeString(*v.AuthorizationToken)
-    if err != nil {
-      break
-    }
+		if err != nil {
+			break
+		}
 
 		splited := strings.SplitN(string(token), ":", 2)
-    if len(splited) != 2 {
-      err = errors.New("Found wrong format")
-      break
-    }
+		if len(splited) != 2 {
+			err = errors.New("Found wrong format")
+			break
+		}
 
-    a := &auth{
-      username: splited[0],
-      password: splited[1],
-			proxy: *(v.ProxyEndpoint),
+		a := &auth{
+			username:   splited[0],
+			password:   splited[1],
+			proxy:      *(v.ProxyEndpoint),
 			expiration: *(v.ExpiresAt),
-    }
+		}
 
-    auths = append(auths, a)
+		auths = append(auths, a)
 	}
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  if len(auths) == 0 {
-    return nil, errors.New("No auths")
-  }
+	if len(auths) == 0 {
+		return nil, errors.New("No auths")
+	}
 
-  return auths, nil
+	return auths, nil
 }
 
 func main() {
-  p, err := getParameter()
-  if err != nil {
-    return
-  }
+	p, err := getParameter()
+	if err != nil {
+		return
+	}
 
-  auths, err := getAuths(p)
-  if err != nil {
-    return
-  }
+	auths, err := getAuths(p)
+	if err != nil {
+		return
+	}
 
-  a := auths[0]
-  cmd := fmt.Sprintf("docker login -u %s -p %s %s", a.username, a.password, a.proxy)
-  fmt.Println(cmd)
+	a := auths[0]
+	cmd := fmt.Sprintf("docker login -u %s -p %s %s", a.username, a.password, a.proxy)
+	fmt.Println(cmd)
 }
